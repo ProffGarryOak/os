ce559db3c58f369c4522d8fd3629746484d1237f
ProffGarryOak <158749389+ProffGarryOak@users.noreply.github.com>
2024-11-28 11:32:28 +0530

    Update sjf.c

--- sjf.c
+++ sjf.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, total_wt = 0, total_tat = 0;
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int processes[n], burst_time[n], wt[n], tat[n], total_wt = 0, total_tat = 0;
+    int processes[n], burst_time[n], arrival_time[n], wt[n], tat[n], completion_time[n];
 
     for (int i = 0; i < n; i++) {
         processes[i] = i + 1;
-        printf("Enter burst time for process %d: ", i + 1);
-        scanf("%d", &burst_time[i]);
+        printf("Enter arrival time and burst time for process %d: ", i + 1);
+        scanf("%d %d", &arrival_time[i], &burst_time[i]);
     }
 
+    // Sort processes by arrival time
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-            if (burst_time[i] > burst_time[j]) {
-                int temp = burst_time[i];
+            if (arrival_time[i] > arrival_time[j]) {
+                // Swap arrival times
+                int temp = arrival_time[i];
+                arrival_time[i] = arrival_time[j];
+                arrival_time[j] = temp;
+
+                // Swap burst times
+                temp = burst_time[i];
                 burst_time[i] = burst_time[j];
                 burst_time[j] = temp;
+
+                // Swap process numbers
                 temp = processes[i];
                 processes[i] = processes[j];
                 processes[j] = temp;
@@ -27,17 +36,27 @@ int main() {
     }
 
     wt[0] = 0;
-    for (int i = 1; i < n; i++)
-        wt[i] = burst_time[i - 1] + wt[i - 1];
-
-    for (int i = 0; i < n; i++)
+    completion_time[0] = arrival_time[0] + burst_time[0];
+    tat[0] = burst_time[0];
+    
+    for (int i = 1; i < n; i++) {
+        if (arrival_time[i] > completion_time[i - 1]) {
+            completion_time[i] = arrival_time[i] + burst_time[i];
+        } else {
+            completion_time[i] = completion_time[i - 1] + burst_time[i];
+        }
+        wt[i] = completion_time[i] - arrival_time[i] - burst_time[i];
+        if (wt[i] < 0) {
+            wt[i] = 0;
+        }
         tat[i] = burst_time[i] + wt[i];
+    }
 
-    printf("Processes  %-12s  %-14s  %-16s\n", "Burst time", "Waiting time", "Turnaround time");
+    printf("Processes  %-12s  %-12s  %-14s  %-16s  %-16s\n", "Arrival time", "Burst time", "Waiting time", "Turnaround time", "Completion time");
     for (int i = 0; i < n; i++) {
         total_wt += wt[i];
         total_tat += tat[i];
-        printf("%-10d  %-12d  %-14d  %-16d\n", processes[i], burst_time[i], wt[i], tat[i]);
+        printf("%-10d  %-12d  %-12d  %-14d  %-16d  %-16d\n", processes[i], arrival_time[i], burst_time[i], wt[i], tat[i], completion_time[i]);
     }
 
     printf("Average waiting time = %.2f\n", (float)total_wt / n);
