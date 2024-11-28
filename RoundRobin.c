6326b821c37209725b29791a0f33567b087c99ef
ProffGarryOak <158749389+ProffGarryOak@users.noreply.github.com>
2024-11-28 11:32:45 +0530

    Update RoundRobin.c

--- RoundRobin.c
+++ RoundRobin.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main() {
     int n, tq, current_time = 0;
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int processes[n], burst_time[n], remaining_time[n], wt[n], tat[n], total_wt = 0, total_tat = 0;
+    int processes[n], burst_time[n], remaining_time[n], arrival_time[n];
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+    bool done[n];
 
     for (int i = 0; i < n; i++) {
         processes[i] = i + 1;
-        printf("Enter burst time for process %d: ", i + 1);
-        scanf("%d", &burst_time[i]);
+        printf("Enter arrival time and burst time for process %d: ", i + 1);
+        scanf("%d %d", &arrival_time[i], &burst_time[i]);
         remaining_time[i] = burst_time[i];
+        done[i] = false;
     }
 
     printf("Enter time quantum: ");
@@ -19,27 +23,32 @@ int main() {
 
     int completed = 0;
     while (completed < n) {
+        bool found = false;
         for (int i = 0; i < n; i++) {
-            if (remaining_time[i] > 0) {
+            if (!done[i] && arrival_time[i] <= current_time) {
+                found = true;
                 if (remaining_time[i] > tq) {
                     current_time += tq;
                     remaining_time[i] -= tq;
                 } else {
                     current_time += remaining_time[i];
-                    wt[i] = current_time - burst_time[i];
                     remaining_time[i] = 0;
-                    tat[i] = wt[i] + burst_time[i];
+                    wt[i] = current_time - burst_time[i] - arrival_time[i];
+                    tat[i] = current_time - arrival_time[i];
                     total_wt += wt[i];
                     total_tat += tat[i];
+                    done[i] = true;
                     completed++;
                 }
             }
         }
+        if (!found)
+            current_time++;
     }
 
-    printf("Processes  %-12s  %-14s  %-16s\n", "Burst time", "Waiting time", "Turnaround time");
+    printf("Processes  %-12s  %-12s  %-14s  %-16s\n", "Arrival time", "Burst time", "Waiting time", "Turnaround time");
     for (int i = 0; i < n; i++) {
-        printf("%-10d  %-12d  %-14d  %-16d\n", processes[i], burst_time[i], wt[i], tat[i]);
+        printf("%-10d  %-12d  %-12d  %-14d  %-16d\n", processes[i], arrival_time[i], burst_time[i], wt[i], tat[i]);
     }
 
     printf("Average waiting time = %.2f\n", (float)total_wt / n);
